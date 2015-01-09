7f9880a442d3c5392fc4be8acbd0c5f882f92246
leorge <qmisort@gmail.com>
2015-01-09 21:39:34 +0900

    Add a program to test Arrays.sort

---
+++ StdSort.java
@@ -0,0 +1,47 @@
+public class StdSort {
+
+    // This class should not be instantiated.
+    private StdSort() { }
+
+    /***********************************************************************
+     *  Helper sorting functions
+     ***********************************************************************/
+     
+     // is v < w ?
+     private static boolean less(Comparable v, Comparable w) {
+         return (v.compareTo(w) < 0);
+     }
+
+    /***********************************************************************
+     *  Check if array is sorted - useful for debugging
+     ***********************************************************************/
+     private static boolean isSorted(Comparable[] a) {
+         return isSorted(a, 0, a.length - 1);
+     }
+
+     private static boolean isSorted(Comparable[] a, int lo, int hi) {
+         for (int i = lo + 1; i <= hi; i++)
+             if (less(a[i], a[i-1])) return false;
+         return true;
+     }
+
+     // print array to standard output
+     private static void show(Comparable[] a) {
+         for (int i = 0; i < a.length; i++) {
+             StdOut.println(a[i]);
+         }
+     }
+
+     /**
+     * Reads in a sequence of strings from standard input; 
+     * and sorts them; and prints them to standard output in ascending order. 
+     */
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Stopwatch when = new Stopwatch();
+        java.util.Arrays.sort(a);
+        double elapsedTime = when.elapsedTime();
+        if (isSorted(a)) StdOut.println("Arrays.Sort sec. = " + elapsedTime);
+        else show(a);
+    }
+}
