c967b79bb2204fc863394d5de52095c31c2728d1
leorge <qmisort@gmail.com>
2015-01-16 12:30:08 +0900

    Delete debug statements.

--- KillQuickX.pl
+++ KillQuickX.pl
@@ -6,20 +6,17 @@ use strict;
 use warnings;
 $#ARGV < 0 && die "Usage : $0 count\n";
 my $n = shift(@ARGV);	# count
-my ($N, $eps) = ($n, 0);
 my $fmt = sprintf "%%0%dd\n", log($n) / log(10) + 1;
 my @insert = (6, 5, 4, 3, 2, 1, 0);	# bad data for insertion sort
 my @list;
 if ($n <= $#insert + 1) {@list = splice(@insert, 0, $n)}
 elsif ($n <= 40) {@list = ($n-1, @insert, 7..$n-2)}	# median-of-3 killer
 else {
-#	my $N = 40;
-	$N = 40;
+	my $N = 40;
 	@list = ($N-1, @insert,7..$N-2);
 	do {
 		$N += 4;	# add 4 elements
-#		my $eps = int($N / 8);
-		$eps = int($N / 8);
+		my $eps = int($N / 8);
 		my @x = splice(@list, 0, 1, $N-3);
 		my @z = splice(@list, $eps, 1, $N-2);
 		my @y = splice(@list, $N - $eps - 1, 1, $N-1);
@@ -27,4 +24,3 @@ else {
 	} while ($#list < $n - 1)
 }
 foreach my $i (@list) {printf $fmt, $i;}
-#print "\$N = $N  \$eps = $eps \$#list = $#list\n";
