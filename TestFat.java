ebc24f18c43998761ab02a98dbe5b6bed05a7741
leorge <qmisort@gmail.com>
2015-01-26 05:27:31 +0900

    Add MergeX to test

--- TestFat.java
+++ TestFat.java
@@ -1,23 +1,25 @@
 public class TestFat {
-	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         String[] a = StdIn.readAllStrings();
         int	N = a.length;
         fatObj[] src = new fatObj[N];
         for (int i = 0; i < N; i++) src[i] = new fatObj(a[i]);
         fatObj[] wrk = src.clone();
         Stopwatch timer = new Stopwatch();
-        java.util.Arrays.sort(wrk);
+        MergeX.sort(wrk);
         double elapsedTime = timer.elapsedTime();
 //        fatObj.show(wrk);
-        if (fatObj.isSorted(wrk)) StdOut.println("Arrays sec. = " + elapsedTime);
+        if (fatObj.isSorted(wrk)) StdOut.println("MergeX sec. = " + elapsedTime);
         wrk = src.clone();
         double startTime = timer.elapsedTime();
         QuickX.sort(wrk);
+        elapsedTime = timer.elapsedTime();
+//      fatObj.show(wrk);
+        if (fatObj.isSorted(wrk)) 
+        	StdOut.println("Arrays sec. = " + ((int)((elapsedTime - startTime) * 1000)) / 1000.);
+        wrk = src.clone();
+        startTime = timer.elapsedTime();
+        java.util.Arrays.sort(wrk);
         elapsedTime = timer.elapsedTime();
 //      fatObj.show(wrk);
         if (fatObj.isSorted(wrk)) 
